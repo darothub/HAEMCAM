d89fb9de3080ae1d12c07a0150ac8df62f8cc570
darot <darotudeen@gmail.com>
2021-04-03 23:15:55 +0100

    fixed instrumented test on home image

--- app/src/androidTest/java/ng/com/thewhitecellfoundation/haemcam/ui/HomeFragmentTest.kt
+++ app/src/androidTest/java/ng/com/thewhitecellfoundation/haemcam/ui/HomeFragmentTest.kt
@@ -1,37 +1,33 @@
 package ng.com.thewhitecellfoundation.haemcam.ui
 
-import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.lifecycle.Lifecycle
 import androidx.test.espresso.Espresso
-import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import ng.com.thewhitecellfoundation.haemcam.R
 import org.hamcrest.Matchers
 import org.junit.Assert.*
-import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class HomeFragmentTest {
-    @Before
-    fun setup() {
-        val scene =
-            launchFragmentInContainer<HomeFragment>(themeResId = R.style.Theme_FullScreen_Translucent)
-        scene.moveToState(Lifecycle.State.RESUMED)
-    }
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun testThatHomeFragment_Is_Displayed_When_Activity_Is_Launched() {
         Espresso.onView(withId(R.id.home_frag_root))
-            .check(matches(isDisplayed()))
+            .check(ViewAssertions.matches(isDisplayed()))
     }
 
     @Test
     fun testHomeImage_Is_Displayed_When_HomeFragment_Is_Launched() {
         Espresso.onView(withId(R.id.home_fragment_iv))
-            .check(matches(isDisplayed()))
+            .check(ViewAssertions.matches(isDisplayed()))
     }
     @Test
     fun testLoginButton_Is_Displayed_When_HomeFragment_Is_Launched() {
@@ -41,7 +37,7 @@ class HomeFragmentTest {
                 withText(R.string.login_str)
             )
         )
-            .check(matches(isDisplayed()))
+            .check(ViewAssertions.matches(isDisplayed()))
     }
     @Test
     fun testSignUpButton_Is_Displayed_When_HomeFragment_Is_Launched() {
@@ -51,6 +47,6 @@ class HomeFragmentTest {
                 withText(R.string.create_an_account)
             )
         )
-            .check(matches(isDisplayed()))
+            .check(ViewAssertions.matches(isDisplayed()))
     }
 }
