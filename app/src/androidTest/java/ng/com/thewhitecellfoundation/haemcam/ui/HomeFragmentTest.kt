09768df53d87113a31f072f525f8071939771f9f
darot <darotudeen@gmail.com>
2021-04-03 22:47:46 +0100

    formatted

--- app/src/androidTest/java/ng/com/thewhitecellfoundation/haemcam/ui/HomeFragmentTest.kt
+++ app/src/androidTest/java/ng/com/thewhitecellfoundation/haemcam/ui/HomeFragmentTest.kt
@@ -2,7 +2,6 @@ package ng.com.thewhitecellfoundation.haemcam.ui
 
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
