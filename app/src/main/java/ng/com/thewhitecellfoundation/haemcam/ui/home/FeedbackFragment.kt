ca6ac8e617fbd4ffd66cb6edafc61d23ef17149e
Oluwafemijohn <femiogundipe01@gmail.com>
2021-08-03 17:33:14 +0100

    before checking out

--- app/src/main/java/ng/com/thewhitecellfoundation/haemcam/ui/home/FeedbackFragment.kt
+++ app/src/main/java/ng/com/thewhitecellfoundation/haemcam/ui/home/FeedbackFragment.kt
@@ -2,6 +2,8 @@ package ng.com.thewhitecellfoundation.haemcam.ui.home
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.fragment.app.Fragment
 import ng.com.thewhitecellfoundation.common.utils.viewBinding
 import ng.com.thewhitecellfoundation.haemcam.R
@@ -14,8 +16,18 @@ import ng.com.thewhitecellfoundation.haemcam.databinding.FragmentFeedbackBinding
  */
 class FeedbackFragment : Fragment(R.layout.fragment_feedback) {
 
+    private lateinit var sicknessSelectorDropdown: AutoCompleteTextView
     private val binding by viewBinding(FragmentFeedbackBinding::bind)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        sicknessSelectorDropdown = binding.categoryDropdownEditText
+    }
+
+    override fun onResume() {
+        super.onResume()
+        /*Set up States Dropdown*/
+        val states = resources.getStringArray(R.array.sickness)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.categorries_drop_down_item, states)
+        sicknessSelectorDropdown.setAdapter(arrayAdapter)
     }
 }
