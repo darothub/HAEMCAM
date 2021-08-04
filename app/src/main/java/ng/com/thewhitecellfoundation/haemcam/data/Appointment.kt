dc062a12b97374f901b09065dfb525f2391c2463
fletcher17 <ororo17@gmail.com>
2021-08-04 16:43:37 +0100

    feature notification fragment

---
+++ app/src/main/java/ng/com/thewhitecellfoundation/haemcam/data/Appointment.kt
@@ -0,0 +1,3 @@
+package ng.com.thewhitecellfoundation.haemcam.data
+
+data class Appointment(val date: String, val appointment: String, val doctorName: String)
---
+++ app/src/main/java/ng/com/thewhitecellfoundation/haemcam/model/Constants.kt
@@ -0,0 +1,9 @@
+package ng.com.thewhitecellfoundation.haemcam.model
+
+import ng.com.thewhitecellfoundation.haemcam.data.Appointment
+
+var DoctorAppointment: ArrayList<Appointment> = arrayListOf(
+    Appointment("4th August, 2021", "Medical Check Up", "Doctor Darot"),
+    Appointment("3rd August, 2021", "Eye Test", "Doctor Dapo"),
+    Appointment("2nd August, 2021", "Back Pain", "Doctor Ola")
+)
