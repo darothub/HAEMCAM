816f68646564452fea59389636b1b0df6b4e6180
Oluwafemijohn <femiogundipe01@gmail.com>
2021-08-04 10:53:52 +0100

    The feedback ui implementation

---
+++ app/src/main/java/ng/com/thewhitecellfoundation/haemcam/model/FeedBackItemData.kt
@@ -0,0 +1,8 @@
+package ng.com.thewhitecellfoundation.haemcam.model
+
+data class FeedBackItemData(
+    var drName: String,
+    var message: String,
+    var numberOfLikes: Int,
+    var numberOfComments: Int
+)
