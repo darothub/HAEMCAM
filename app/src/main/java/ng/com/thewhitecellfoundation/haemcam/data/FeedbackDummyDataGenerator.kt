49a276a06cc33accb23e2b242d35890c11300e0e
Paul-wiseman <paulndibe92@gmail.com>
2021-08-04 13:14:16 +0100

    Implemented FeedbackFragment features

---
+++ app/src/main/java/ng/com/thewhitecellfoundation/haemcam/data/FeedbackDummyDataGenerator.kt
@@ -0,0 +1,119 @@
+package ng.com.thewhitecellfoundation.haemcam.data
+
+import ng.com.thewhitecellfoundation.haemcam.model.FeedBack
+
+object FeedbackDummyDataGenerator {
+
+    fun createFeedbackData(): ArrayList<FeedBack> {
+
+        return arrayListOf(
+            FeedBack(
+                "Dr Anthonia Peterson",
+                "Here are some medical feedback.... Here are\n" +
+                    "some medical feedback....Here are some\n" +
+                    "medical feedback....Here are some medical\n" +
+                    "feedback....",
+                23,
+                4
+            ),
+            FeedBack(
+                "Dr Darot",
+                "Here are some medical feedback.... Here are\n" +
+                    "some medical feedback....Here are some\n" +
+                    "medical feedback....Here are some medical\n" +
+                    "feedback....",
+                59,
+                30
+            ),
+            FeedBack(
+                "Dr Olalekan",
+                "Here are some medical feedback.... Here are\n" +
+                    "some medical feedback....Here are some\n" +
+                    "medical feedback....Here are some medical\n" +
+                    "feedback....",
+                23,
+                8
+            ),
+
+            FeedBack(
+                "Dr Ruth Onoka",
+                "Here are some medical feedback.... Here are\n" +
+                    "some medical feedback....Here are some\n" +
+                    "medical feedback....Here are some medical\n" +
+                    "feedback....",
+                6,
+                55
+            ),
+
+            FeedBack(
+                "Dr Wilson Ahanmisi",
+                "Here are some medical feedback.... Here are\n" +
+                    "some medical feedback....Here are some\n" +
+                    "medical feedback....Here are some medical\n" +
+                    "feedback....",
+                23,
+                67
+            ),
+
+            FeedBack(
+                "Dr Jesse Onoyeyan",
+                "Here are some medical feedback.... Here are\n" +
+                    "some medical feedback....Here are some\n" +
+                    "medical feedback....Here are some medical\n" +
+                    "feedback....",
+                70,
+                200
+            ),
+
+            FeedBack(
+                "Dr Adebayo SmallBoy",
+                "Here are some medical feedback.... Here are\n" +
+                    "some medical feedback....Here are some\n" +
+                    "medical feedback....Here are some medical\n" +
+                    "feedback....",
+                0,
+                10
+            ),
+
+            FeedBack(
+                "Dr Fatima",
+                "Here are some medical feedback.... Here are\n" +
+                    "some medical feedback....Here are some\n" +
+                    "medical feedback....Here are some medical\n" +
+                    "feedback....",
+                12,
+                26
+            ),
+
+            FeedBack(
+                "Dr Dennis Illuma",
+                "Here are some medical feedback.... Here are\n" +
+                    "some medical feedback....Here are some\n" +
+                    "medical feedback....Here are some medical\n" +
+                    "feedback....",
+                23,
+                40
+            ),
+
+            FeedBack(
+                "Dr WhiteMoney",
+                "Here are some medical feedback.... Here are\n" +
+                    "some medical feedback....Here are some\n" +
+                    "medical feedback....Here are some medical\n" +
+                    "feedback....",
+                230,
+                400
+            ),
+            FeedBack(
+                "Dr Anthonia Peterson",
+                "Here are some medical feedback.... Here are\n" +
+                    "some medical feedback....Here are some\n" +
+                    "medical feedback....Here are some medical\n" +
+                    "feedback....",
+                23,
+                4
+            )
+
+        )
+    }
+}
